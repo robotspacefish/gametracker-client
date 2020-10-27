56d26b0b2bbc23f5e74a2137d51814d2b7069662
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-27 15:56:11 -0400

    Welcome page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header/Header';
+import Welcome from './Welcome/Welcome';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <Header />
 
+      {/* TODO: if no user is logged in, show welcome page at '/'
+      otherwise, re-route to user's collection */}
+      <Welcome />
     </div>
   );
 }
---
+++ src/Welcome/Welcome.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div className="Welcome">
+      <h2>Keep track of your ever-growing game collection</h2>
+
+      <p>With an account, you can:</p>
+      <ul>
+        <li>Search for games to add to your collection</li>
+        <li>Add notes/objectives so you never have to forget where you left off again</li>
+        <li>See what other users are playing</li>
+        <li>Have your collection analyzed to decide what you should play next</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Welcome;
